403b0cea26b3ecff6bc03eb4be7c722f3bf60509
swaggboi <swaggboi@slackware.uk>
2022-08-22 12:27:08 -0400

    Forgot a few things here

--- migrations/5/down.sql
+++ migrations/5/down.sql
@@ -2,13 +2,27 @@
 RENAME TO replies;
 
  ALTER TABLE replies
-RENAME remark_id TO reply_id;
+RENAME remark_id
+    TO reply_id;
 
  ALTER TABLE replies
-RENAME remark_date TO reply_date;
+RENAME remark_date
+    TO reply_date;
 
  ALTER TABLE replies
-RENAME remark_author TO reply_author;
+RENAME remark_author
+    TO reply_author;
 
  ALTER TABLE replies
-RENAME remark_body TO reply_body;
+RENAME remark_body
+    TO reply_body;
+
+ ALTER TABLE replies
+RENAME CONSTRAINT remarks_thread_id_fkey
+    TO replies_thread_id_fkey;
+
+ ALTER INDEX remarks_pkey
+RENAME TO replies_pkey;
+
+ ALTER SEQUENCE remarks_remark_id_seq
+RENAME TO replies_reply_id_seq;
--- migrations/5/up.sql
+++ migrations/5/up.sql
@@ -2,13 +2,27 @@
 RENAME TO remarks;
 
  ALTER TABLE remarks
-RENAME reply_id TO remark_id;
+RENAME reply_id
+    TO remark_id;
 
  ALTER TABLE remarks
-RENAME reply_date TO remark_date;
+RENAME reply_date
+    TO remark_date;
 
  ALTER TABLE remarks
-RENAME reply_author TO remark_author;
+RENAME reply_author
+   TO remark_author;
 
  ALTER TABLE remarks
-RENAME reply_body TO remark_body;
+RENAME reply_body
+    TO remark_body;
+
+ ALTER TABLE remarks
+RENAME CONSTRAINT replies_thread_id_fkey
+    TO remarks_thread_id_fkey;
+
+ ALTER INDEX replies_pkey
+RENAME TO remarks_pkey;
+
+ ALTER SEQUENCE replies_reply_id_seq
+RENAME TO remarks_remark_id_seq;
