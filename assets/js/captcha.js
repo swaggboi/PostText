0a2c198d1bd7dc1f083b7f27ab23446ca4dc6a7c
swag <swaggboi@slackware.uk>
2023-06-14 18:49:30 -0400

    Automate the captcha

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -1,13 +1,20 @@
-/*
 'use strict';
 
-document.getElementById('captcha').value =
-*/
-
-'use strict';
-
-let captchaValue =
-    document
+(() => {
+    let captchaValue = document
         .querySelector('label[for="captcha"]')
         .textContent
         .match(/'(flag|bump)'/)[1];
+    let captchaForm = document
+        .querySelector('form[class="form-body"]');
+
+    captchaForm.captcha.value = captchaValue;
+    // Make 'em work for it
+    window.crypto.subtle.generateKey({
+        name: 'RSA-OAEP',
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([1, 0, 1]),
+        hash: 'SHA-512'
+    }, false, ['encrypt', 'decrypt'])
+        .finally(() => captchaForm.submit());
+})();
