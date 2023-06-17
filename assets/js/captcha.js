0b7509d6eeb9e88130b094d4ff398f4c9c3e9dc8
swag <swaggboi@slackware.uk>
2023-06-17 18:10:19 -0400

    Revert "Change the 'business logic' here"
    This reverts commit 62673f5aec214d1e2ffa88ff2f6a2003c23ea493.

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -1,13 +1,6 @@
 'use strict';
 
 (() => {
-    // Make 'em work for it!
-    let doCryptoStuff = window.crypto.subtle.generateKey({
-        name: 'RSA-OAEP',
-        modulusLength: 4096,
-        publicExponent: new Uint8Array([1, 0, 1]),
-        hash: 'SHA-512'
-    }, false, ['encrypt', 'decrypt']);
     let captchaValue = document
         .querySelector('label[for="captcha"]')
         .textContent
@@ -15,17 +8,13 @@
     let captchaForm = document
         .querySelector('form[class="form-body"]');
 
-    captchaForm.captcha.readOnly = true;
-    captchaForm.captcha.value = '⏳';
-
-    doCryptoStuff
-        .then(() => {
-            captchaForm.captcha.value = captchaValue;
-            captchaForm.submit();
-        })
-        .catch((err) => {
-            captchaForm.captcha.value = null;
-            captchaForm.captcha.readOnly = false;
-            console.log(err);
-        });
+    captchaForm.captcha.value = captchaValue;
+    // Make 'em work for it
+    window.crypto.subtle.generateKey({
+        name: 'RSA-OAEP',
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([1, 0, 1]),
+        hash: 'SHA-512'
+    }, false, ['encrypt', 'decrypt'])
+        .finally(() => captchaForm.submit());
 })();
