1cff4f3cf1011e25a59d984d79fa29dc863d655a
swag <swaggboi@slackware.uk>
2023-06-17 18:19:01 -0400

    Revert "Automate the captcha"
    This reverts commit 0a2c198d1bd7dc1f083b7f27ab23446ca4dc6a7c.

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -1,20 +1,13 @@
+/*
 'use strict';
 
-(() => {
-    let captchaValue = document
+document.getElementById('captcha').value =
+*/
+
+'use strict';
+
+let captchaValue =
+    document
         .querySelector('label[for="captcha"]')
         .textContent
         .match(/'(flag|bump)'/)[1];
-    let captchaForm = document
-        .querySelector('form[class="form-body"]');
-
-    captchaForm.captcha.value = captchaValue;
-    // Make 'em work for it
-    window.crypto.subtle.generateKey({
-        name: 'RSA-OAEP',
-        modulusLength: 4096,
-        publicExponent: new Uint8Array([1, 0, 1]),
-        hash: 'SHA-512'
-    }, false, ['encrypt', 'decrypt'])
-        .finally(() => captchaForm.submit());
-})();
