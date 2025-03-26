2cbe24584b5464d202b2ed98b900b63d0103d238
swaggboi <swaggboi@gangstalking.agency>
2025-03-26 14:38:24 -0400

    Add new migrations

---
+++ migrations/16/down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE threads
+ DROP COLUMN markdown_status;
+
+ALTER TABLE remarks
+ DROP COLUMN markdown_status;
---
+++ migrations/16/up.sql
@@ -0,0 +1,23 @@
+ALTER TABLE threads
+  ADD COLUMN markdown_status BOOLEAN;
+
+-- Since Markdown was default, set existing threads to true
+UPDATE threads
+   SET markdown_status = TRUE;
+
+-- Now we can make it NOT NULL
+ALTER TABLE threads
+ALTER COLUMN markdown_status
+  SET NOT NULL;
+
+-- Do the same for remarks
+
+ALTER TABLE remarks
+  ADD COLUMN markdown_status BOOLEAN;
+
+UPDATE remarks
+   SET markdown_status = TRUE;
+
+ALTER TABLE remarks
+ALTER COLUMN markdown_status
+  SET NOT NULL;
